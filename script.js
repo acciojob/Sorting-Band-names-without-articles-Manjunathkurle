650cc011914af11ccceb85a4668fce69192dcb6a
Manjunathkurle <127331236+Manjunathkurle@users.noreply.github.com>
2023-09-15 12:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+let bandNames = ['The Beatles', 'Aerosmith', 'The Rolling Stones', 'Led Zeppelin'];
+
+// Function to remove articles ('a', 'an', 'the') and return the modified string
+function removeArticles(name) {
+  return name.replace(/^(a|an|the) /i, '').trim();
+}
+
+// Sort the array without articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Get the ul element by id
+const ulElement = document.getElementById('band');
+
+// Loop through the sorted band names and create li elements to append to the ul
+bandNames.forEach((bandName) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
+
 
